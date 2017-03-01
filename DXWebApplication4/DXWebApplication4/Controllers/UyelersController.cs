1f583120175eb8fb351cb4f80cb9387969d96fd7
AlperAkyldz <akyildizal@itu.edu.tr>
2017-03-01 20:13:08 +0400

    New Template

---
+++ DXWebApplication4/DXWebApplication4/Controllers/UyelersController.cs
@@ -0,0 +1,207 @@
+﻿using DevExpress.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DXWebApplication4.Models;
+
+namespace DXWebApplication4.Controllers
+{
+    public class UyelersController : Controller
+    {
+        private NeredeYesekDBEntities db = new NeredeYesekDBEntities();
+
+        // GET: Uyelers
+        public ActionResult Index()
+        {
+            var uyelers = db.Uyelers.Include(u => u.Grup);
+            return View(uyelers.ToList());
+        }
+
+        // GET: Uyelers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uyeler uyeler = db.Uyelers.Find(id);
+            if (uyeler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uyeler);
+        }
+
+        // GET: Uyelers/Create
+        public ActionResult Create()
+        {
+            ViewBag.GID = new SelectList(db.Grups, "GID", "GrupAdi");
+            return View();
+        }
+
+        // POST: Uyelers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "UID,GID,Isim,SoyIsim,Email")] Uyeler uyeler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Uyelers.Add(uyeler);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GID = new SelectList(db.Grups, "GID", "GrupAdi", uyeler.GID);
+            return View(uyeler);
+        }
+
+        // GET: Uyelers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uyeler uyeler = db.Uyelers.Find(id);
+            if (uyeler == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GID = new SelectList(db.Grups, "GID", "GrupAdi", uyeler.GID);
+            return View(uyeler);
+        }
+
+        // POST: Uyelers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "UID,GID,Isim,SoyIsim,Email")] Uyeler uyeler)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(uyeler).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GID = new SelectList(db.Grups, "GID", "GrupAdi", uyeler.GID);
+            return View(uyeler);
+        }
+
+        // GET: Uyelers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Uyeler uyeler = db.Uyelers.Find(id);
+            if (uyeler == null)
+            {
+                return HttpNotFound();
+            }
+            return View(uyeler);
+        }
+
+        // POST: Uyelers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Uyeler uyeler = db.Uyelers.Find(id);
+            db.Uyelers.Remove(uyeler);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        DXWebApplication4.Models.NeredeYesekDBEntities db1 = new DXWebApplication4.Models.NeredeYesekDBEntities();
+
+        [ValidateInput(false)]
+        public ActionResult GridView1Partial()
+        {
+            var model = db1.Grups;
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialAddNew(DXWebApplication4.Models.Grup item)
+        {
+            var model = db1.Grups;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    model.Add(item);
+                    db1.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialUpdate(DXWebApplication4.Models.Grup item)
+        {
+            var model = db1.Grups;
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var modelItem = model.FirstOrDefault(it => it.GID == item.GID);
+                    if (modelItem != null)
+                    {
+                        this.UpdateModel(modelItem);
+                        db1.SaveChanges();
+                    }
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            else
+                ViewData["EditError"] = "Please, correct all errors.";
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+        [HttpPost, ValidateInput(false)]
+        public ActionResult GridView1PartialDelete(System.Int32 GID)
+        {
+            var model = db1.Grups;
+            if (GID >= 0)
+            {
+                try
+                {
+                    var item = model.FirstOrDefault(it => it.GID == GID);
+                    if (item != null)
+                        model.Remove(item);
+                    db1.SaveChanges();
+                }
+                catch (Exception e)
+                {
+                    ViewData["EditError"] = e.Message;
+                }
+            }
+            return PartialView("_GridView1Partial", model.ToList());
+        }
+    }
+}
---
+++ DXWebApplication4/DXWebApplication4/Controllers/İstatistiklerController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DXWebApplication4.Controllers
+{
+    public class İstatistiklerController : Controller
+    {
+        // GET: İstatistikler
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
