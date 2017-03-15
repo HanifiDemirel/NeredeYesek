b5d090caf0a44a75d6cf929308683a1240dfb3df
AlperAkyldz <akyildizal@itu.edu.tr>
2017-03-15 13:15:08 +0400

    dropdown list added

--- ContosoUniversity/ContosoUniversity/Schedulers/SchedulerContainer.cs
+++ ContosoUniversity/ContosoUniversity/Schedulers/SchedulerContainer.cs
@@ -19,7 +19,7 @@ namespace ContosoUniversity.Schedulers
 
                 IJobDetail jobMonthly = JobBuilder.Create<MonthlyScheduler>().WithIdentity("MonthlyScheduler", null).Build();
                 //ISimpleTrigger triggerMonthly = (ISimpleTrigger)TriggerBuilder.Create().WithIdentity("MonthlyScheduler").StartAt(DateTime.Now.AddMonths(2)).WithSimpleSchedule(x => x.WithIntervalInSeconds(100).RepeatForever()).Build();
-                ISimpleTrigger triggerMonthly = (ISimpleTrigger)TriggerBuilder.Create().WithIdentity("MonthlyScheduler").StartAt(DateTime.Now.AddSeconds(3)).WithSimpleSchedule(x => x.WithIntervalInSeconds(200).RepeatForever()).Build();
+                ISimpleTrigger triggerMonthly = (ISimpleTrigger)TriggerBuilder.Create().WithIdentity("MonthlyScheduler").StartAt(DateTime.Now.AddSeconds(300)).WithSimpleSchedule(x => x.WithIntervalInSeconds(200).RepeatForever()).Build();
 
                 sched.ScheduleJob(jobMonthly, triggerMonthly);
 
@@ -27,7 +27,7 @@ namespace ContosoUniversity.Schedulers
 
 
                 IJobDetail jobDaily = JobBuilder.Create<DailyScheduler>().WithIdentity("DailyScheduler", null).Build();
-                ISimpleTrigger triggerDaily = (ISimpleTrigger)TriggerBuilder.Create().WithIdentity("DailyScheduler").StartAt(DateTime.Now.AddSeconds(5)).WithSimpleSchedule(x => x.WithIntervalInSeconds(3).RepeatForever()).Build();
+                ISimpleTrigger triggerDaily = (ISimpleTrigger)TriggerBuilder.Create().WithIdentity("DailyScheduler").StartAt(DateTime.Now.AddSeconds(500)).WithSimpleSchedule(x => x.WithIntervalInSeconds(3).RepeatForever()).Build();
                 sched.ScheduleJob(jobDaily, triggerDaily);
             }
             catch (Exception ex)
